71f2c7a4e2c5aac6c22aac7b6f39f32d49544be5
Layerex <layerex@dismail.de>
2022-02-20 20:56:35 +0300

    Redraw UI elements only if needed
    This prevents flickering in some terminals
    Also I refactored code a bit

--- curses_app.py
+++ curses_app.py
@@ -7,57 +7,92 @@ class CursesApp:
 
     KEYBIND_TEXT = "[j] Decrease WPM [k] Increase WPM [Space] Play/Pause [q] Quit"
     HORIZONAL_SEP_LENGTH = 21
-    HORIZONTAL_SEP = '⎯' * HORIZONAL_SEP_LENGTH
-    ORP_INDICATOR_TOP = '▼'
-    ORP_INDICATOR_BOT = '▲'
+    HORIZONTAL_SEP = "⎯" * HORIZONAL_SEP_LENGTH
+    ORP_INDICATOR_TOP = "▼"
+    ORP_INDICATOR_BOT = "▲"
 
     def __init__(self, filename):
         self.document = Document(filename)
+        self.words = self.document.read_document()
 
     def application(self, stdscr):
         """Main application"""
+
         stdscr.nodelay(True)
         stdscr.clear()
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
 
-        words = self.document.read_document()
+        def draw_keybind_text():
+            middle_keybind = x_mid - len(self.KEYBIND_TEXT) // 2
+            stdscr.addstr(curses.LINES - 1, middle_keybind, self.KEYBIND_TEXT)
 
-        for word, orp_ind in words:
+        def draw_wpm():
+            info_text = f"WPM: {self.document.wpm}"
+            stdscr.addstr(curses.LINES - 3, x_mid - len(info_text) // 2, info_text)
+
+        def draw_borders():
+            stdscr.addstr(y_mid - 1, x_mid - 10, self.HORIZONTAL_SEP)
+            stdscr.addstr(
+                y_mid - 1, x_mid, self.ORP_INDICATOR_TOP, curses.color_pair(1)
+            )
+            stdscr.addstr(y_mid + 1, x_mid - 10, self.HORIZONTAL_SEP)
+            stdscr.addstr(
+                y_mid + 1, x_mid, self.ORP_INDICATOR_BOT, curses.color_pair(1)
+            )
+
+        def draw_word():
+            stdscr.move(y_mid, 0)
+            stdscr.clrtoeol()
+            stdscr.addstr(y_mid, x_mid - orp_ind, word[:orp_ind])
+            stdscr.addstr(y_mid, x_mid, word[orp_ind], curses.color_pair(1))
+            stdscr.addstr(y_mid, x_mid + 1, word[orp_ind + 1 :])
+
+        def move_cursor():
+            stdscr.move(curses.LINES - 1, curses.COLS - 1)
+
+        def redraw():
+            if redraw_needed:
+                stdscr.clear()
+                draw_borders()
+                draw_keybind_text()
+            if redraw_needed or wpm_redraw_needed:
+                draw_wpm()
+            draw_word()
+            move_cursor()
+
+        rows = cols = 0
+        first_iteration = True
+
+        for word, orp_ind in self.words:
             c = stdscr.getch()
             curses.flushinp()
-            stdscr.clear()
             curses.update_lines_cols()
 
-            if c == ord('q'):
+            x_mid = curses.COLS // 2
+            y_mid = curses.LINES // 2
+            redraw_needed = (
+                rows != curses.COLS or cols != curses.LINES or first_iteration
+            )
+            rows = curses.COLS
+            cols = curses.LINES
+
+            wpm_redraw_needed = False
+            if c == ord("q"):
                 break
-            elif c == ord('k'):
+            elif c == ord("k"):
                 self.document.wpm += 50
-            elif c == ord('j'):
+                wpm_redraw_needed = True
+            elif c == ord("j"):
                 self.document.wpm -= 50
-            elif c == ord(' '):
+                wpm_redraw_needed = True
+            elif c == ord(" "):
                 self.document.toggle_play_pause()
 
-            x_mid = int(curses.COLS / 2)
-            y_mid = int(curses.LINES / 2)
+            redraw()
 
-            info_text = f'WPM: {self.document.wpm}'
-            stdscr.addstr(curses.LINES - 3, x_mid - int(len(info_text)/2), info_text)
-
-            middle_keybind = x_mid - int((len(self.KEYBIND_TEXT)/2))
-            stdscr.addstr(int(curses.LINES - 1), middle_keybind, self.KEYBIND_TEXT)
-
-            stdscr.addstr(y_mid - 1, x_mid - 10, self.HORIZONTAL_SEP)
-            stdscr.addstr(y_mid - 1, x_mid, self.ORP_INDICATOR_TOP, \
-                          curses.color_pair(1))
-            stdscr.addstr(y_mid + 1, x_mid - 10, self.HORIZONTAL_SEP)
-            stdscr.addstr(y_mid + 1, x_mid, self.ORP_INDICATOR_BOT, \
-                          curses.color_pair(1))
-
-            stdscr.addstr(y_mid, int(x_mid - orp_ind), word[:orp_ind])
-            stdscr.addstr(y_mid, x_mid, word[orp_ind], curses.color_pair(1))
-            stdscr.addstr(y_mid, x_mid + 1, word[orp_ind + 1:])
-
-            stdscr.addstr(int(curses.LINES-1), int(curses.COLS-1), '')
+            redraw_needed = False
+            wpm_redraw_needed = False
+            first_iteration = False
 
     def run(self):
         curses.wrapper(self.application)
